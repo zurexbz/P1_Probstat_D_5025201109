45d3d7975094796c77158e4abe3dbe5cf0dace71
zurexbz <83849481+zurexbz@users.noreply.github.com>
2022-04-10 17:28:53 +0700

    Soal no 4

---
+++ P1_soal4.R
@@ -0,0 +1,27 @@
+# a. Diket x = 2 dan v = 10, fungsi probabilitas chi square
+x = 2
+v = 10
+
+p <- dchisq(x, v)
+p
+
+# b. Histogram chi square dengan 100 data random
+v = 10
+x = 100
+
+hist(rchisq(n, v), 
+     main = "Histogram Chi Square",
+     xlim = c(0,20),
+     col = "green",
+     xlab = "X",
+     ylab = "V",
+)
+
+# C. nilai rataan dan varians
+v = 10
+
+mean = v
+mean
+
+varians = 2*v
+varians
